c4a15a149f5ba34041720f594c34212522246ebc
stevieb669 <stevieb669@gmail.com>
2025-04-22 18:09:53 +1200

    Adding HesedEggs

--- ServerApp.js
+++ ServerApp.js
@@ -13,6 +13,11 @@ app.get("/", (req, res, next) => {
    console.log("I received a get request on the path // ")
    res.send("<h1>Acknowledged</h1>")
  })
+
+ app.get("/Home", (req, res, next) => {
+    console.log("I received a get request on the path // ")
+    res.send("<h1> 'style: color: red' Welcome Home</h1>")
+  })
  
 app.listen(port, () => {
     console.log('Example app Listening on port ${port}')
